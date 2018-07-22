643c8b745d74ec5f725a234ab83992971fbe5d46
cketti <cketti@gmail.com>
2018-07-22 15:37:54 +0200

    Move folder list sync logic into Backend implementations

--- backend/api/src/main/java/com/fsck/k9/backend/api/BackendStorage.kt
+++ backend/api/src/main/java/com/fsck/k9/backend/api/BackendStorage.kt
@@ -2,4 +2,10 @@ package com.fsck.k9.backend.api
 
 interface BackendStorage {
     fun getFolder(folderServerId: String): BackendFolder
+
+    fun getFolderServerIds(): List<String>
+
+    fun createFolders(folders: List<FolderInfo>)
+    fun deleteFolders(folderServerIds: List<String>)
+    fun changeFolder(folderServerId: String, name: String)
 }
