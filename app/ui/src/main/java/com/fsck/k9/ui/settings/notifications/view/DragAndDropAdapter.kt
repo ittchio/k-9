abaaa2fb32493d5b8225e8958be81933b9d97589
ittchio <annabella.milani@gmail.com>
2018-07-29 11:37:17 +0100

    Implement DragAndDropRecyclerView

--- app/ui/src/main/java/com/fsck/k9/ui/settings/notifications/view/DragAndDropAdapter.kt
+++ app/ui/src/main/java/com/fsck/k9/ui/settings/notifications/view/DragAndDropAdapter.kt
@@ -1,17 +1,17 @@
 package com.fsck.k9.ui.settings.notifications.view
 
 import android.support.v7.widget.RecyclerView
-import android.view.ViewGroup
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import com.fsck.k9.ui.R
 
 
 class DragAndDropAdapter : RecyclerView.Adapter<DragAndDropViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DragAndDropViewHolder {
-        val mView = LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.drag_and_drop_item, parent, false)
-        return DragAndDropViewHolder(mView)
+        val layoutInflater = LayoutInflater.from(parent.getContext())
+        val view = layoutInflater.inflate(R.layout.drag_and_drop_item, parent, false)
+        return DragAndDropViewHolder(view)
     }
 
     override fun getItemCount() = 20
--- app/ui/src/main/java/com/fsck/k9/ui/settings/notifications/view/DragAndDropRecyclerView.kt
+++ app/ui/src/main/java/com/fsck/k9/ui/settings/notifications/view/DragAndDropRecyclerView.kt
@@ -1,13 +1,17 @@
 package com.fsck.k9.ui.settings.notifications.view
 
 import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
 import android.util.AttributeSet
 
 class DragAndDropRecyclerView(context: Context, attributeSet: AttributeSet) :
     RecyclerView(context, attributeSet) {
 
     init {
-
+        adapter = DragAndDropAdapter()
+        layoutManager = LinearLayoutManager(context)
+        ItemTouchHelper(ItemTouchHelperCallback(adapter)).attachToRecyclerView(this)
     }
 }
